f392ea05dfcf15cbd2d9684a4ebcdff3abeccc48
Shubham Gagare <shubhamgagare1997@gmail.com>
2024-09-24 00:36:55 +0530

    Updated analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 import { ThemeProvider } from "./provider";
 
@@ -26,6 +26,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
